e65892137f7334491c84013a85c8c878996301ca
sbrink00 <sbrink00@stuy.edu>
2018-10-23 20:21:53 -0400

    wrote everything up to resize

--- SuperArray.java
+++ SuperArray.java
@@ -13,13 +13,14 @@ public class SuperArray{
     return this.size;
   }
 
+  public boolean isEmpty(){
+    return this.size() == 0;
+  }
+
   public boolean add(String element){
-    for (int idx = 0; idx < this.size(); idx ++){
-      if (data[idx] == null) {
-        data[idx] = element;
-        return true;
-      }
-    }
+    data[size] = element;
+    String output = data[size];
+    size += 1;
     return true;
   }
 
@@ -28,6 +29,37 @@ public class SuperArray{
     for (int idx = 0; idx < this.size(); idx ++){
       output += data[idx] + ", ";
     }
+    return output.substring(0, output.length() - 2) + "]";
   }
-    return output.substring()
+
+  public String toStringDebug(){
+    String output = "[";
+    for (int idx = 0; idx < 10; idx ++){
+      output += data[idx] + ", ";
+    }
+    return output.substring(0, output.length() - 2) + "]";
+  }
+
+  public String get(int index){
+    if (index > -1 && index < this.size()){
+      return data[index];
+    }
+    else return "if the index is out of range, that is when: (index < 0 || index >= size())";
+  }
+
+  public String set(int index, String element){
+    String output = data[index];
+    if (index > -1 && index < this.size()){
+      data[index] = element;
+    }
+    else return "if the index is out of range, that is when: (index < 0 || index >= size())";
+    return output;
+  }
+
+  /*private void resize(){
+    SuperArray output = new SuperArray[data.length + 1];
+  }
+  */
+
+
 }
